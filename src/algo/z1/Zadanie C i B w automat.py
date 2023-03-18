c5eb3b9e663d2e3321d9043b049a2c061b5df3be
airnomad3 <atreuz002@gmail.com>
2023-03-18 10:03:54 +0100

    Zadanie C i B w automat poprawione bo było źle

--- src/algo/z1/Zadanie C i B w automat.py
+++ src/algo/z1/Zadanie C i B w automat.py
@@ -7,19 +7,19 @@ from random import seed, randint
 def generate_data(data_size):
     # tutaj wstawić coś co generuje dane dla naszego algorytmu
     # return [randint(0, 10 ** 6) for _ in range(data_size)]
-    a = [randint(0, 100) for i in range(data_size)]
-    b = [randint(0, 100) for i in range(data_size)]
-    return a, b
+    a = [randint(1, 100) for i in range(data_size)]
+    b = [randint(1, 100) for i in range(data_size)]
+    print("a: ", a)
+    print("b: ", b)
+    data = a, b
+    return data
 
-
-a, b = generate_data(4)
-
-print("a: ", a)
-print("b: ", b)
 pass
 
 
 def solve_problem(data):
+ a= data[0]
+ b= data[1]
  wynik1 = []
  for i in a:
         wynik = 0
@@ -35,7 +35,7 @@ def run_tests(generator, solver):
     size = 10
     sizes = []
     times = []
-    while size < 10000:
+    while size < 1281:
         print(f'testing solver for {size=}')
         data = generator(size)
         REPETITIONS = 400
