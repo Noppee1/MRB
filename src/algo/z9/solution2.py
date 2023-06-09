58444e0a52c65e5e393cf2494df40b313be285f7
Noppee1 <mkolber03@gmail.com>
2023-06-09 17:04:19 +0200

    Z9 Solution

--- src/algo/z9/solution2.py
+++ src/algo/z9/solution2.py
@@ -1,5 +1,5 @@
 """
-Semantic versioning
+Semanotic versining
 https://devopedia.org/images/article/279/7179.1593248779.png
 
 {Major}.{Minor}.{Patch}
@@ -16,8 +16,8 @@ def get_latest(versions: list[str]) -> str:
     """
     :return: Latest semantic version from the given `versions`
     """
-    # todo: your code
-    return '0.0.1'
+
+    return max(versions)
 
 
 def next_version(version: str, level: int) -> str:
@@ -26,5 +26,21 @@ def next_version(version: str, level: int) -> str:
     :param level: Which part should be incremented; 0: major, 1: minor, 2: patch
     :return: Properly incremented version
     """
-    # todo: your code
-    return '0.0.1'
+
+    a, b, c = version.split(".")
+
+    if level == 0:
+        a = str(int(a) + 1)
+        b = "0"
+        c = "0"
+
+    if level == 1:
+        b = str(int(b) + 1)
+        c = "0"
+
+    if level == 2:
+        c = str(int(c) + 1)
+
+    wyjscie = '.'.join([a, b, c])
+    return wyjscie
+
