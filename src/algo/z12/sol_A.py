7effd122f45ac5f9fdc78456abcd0e94c723c6fb
Noppee1 <mkolber03@gmail.com>
2023-06-30 17:45:23 +0200

    B and A, but A - 1 test broken

--- src/algo/z12/sol_A.py
+++ src/algo/z12/sol_A.py
@@ -1,3 +1,19 @@
 def get_min_number_of_operations(a: list[int]) -> int:
-    # todo: your code here
-    return 0
+
+    counter = 0
+    i = 1
+
+    if a[0] <= 0:
+        counter += 1
+
+    while len(a) > i:
+
+        if a[i] < 0:
+            if a[i - 1] <= 0:
+                i += 1
+                continue
+            counter += 1
+            i += 1
+            continue
+        i += 1
+    return counter
--- src/algo/z12/sol_B.py
+++ src/algo/z12/sol_B.py
@@ -2,8 +2,20 @@
 
 
 def get_best_value_package(costs: list[int], values: list[int], max_total_cost: int) -> int:
-    # znaleźć najlepszą możliwą wartość podzbioru itemów, ale takich by łączny "cost" tych itemów nie przekraczał
-    # wartości max_total_cost
-    pass
+    N = len(costs)
+    wynik = 0
 
+    for k in range(2 ** N):
+        koszt = 0
+        wartosc = 0
+
+        for i in range(N):
+            if k & (1 << i):
+                koszt += costs[i]
+                wartosc += values[i]
+
+        if koszt <= max_total_cost and wartosc > wynik:
+            wynik = wartosc
+
+    return wynik
 
